7a0623fdb01ee664f0d197df11c641fd75c10620
Abir <a00128@reading-school.co.uk>
2026-05-07 17:02:10 +0100

    Update Scrolling_Background.py

--- Scrolling_Background.py
+++ Scrolling_Background.py
@@ -5,19 +5,20 @@ pygame.init()
 screen = pygame.display.set_mode((500, 500), FULLSCREEN)
 
 running = True
-
 while running:
-	
 	keys = pygame.key.get_pressed()
 	
 	for event in pygame.event.get(): 
 		if event.type == pygame.QUIT: 
 			running = False
-			
+
 		if keys[pygame.K_ESCAPE]:
 			running = False
-		
-		if keys[pygame.K_w]:
+			# bare in mind we need WASD and arrow, arrow for players who dont use WASD (cough cough, Me)
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+			screen.fill(pygame.Color(255, 255, 123))
+			
+		if keys[pygame.K_s] or keys[pygame.K_DOWN]:
 			screen.fill(pygame.Color(255, 255, 123))
 	
 	pygame.display.update()
